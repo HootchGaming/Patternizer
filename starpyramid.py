76764d83f2e3ec577077a2f82c8e9d140c17b321
Prithvin Roby <69177845+HootchGaming@users.noreply.github.com>
2020-09-13 09:47:34 +0530

    Update starpyramid.py

--- starpyramid.py
+++ starpyramid.py
@@ -1,16 +1,50 @@
 while True:
     try:
-        rows1 = int(input("Enter a number: "))
-        if rows1 > 1 and rows1 <= 100:
-            for i in range(0, rows1):
-                for j in range(0, rows1-i-1):
-                    print(end=" ")
-                for j in range(0, i+1):
-                    print("*", end=" ")
-                print()
+        a = int(input("Enter 1 for Star Pyramid or Enter 2 for Upside Down Half Pyramid : "))
+
+
+        if a == 2:
+            while True:
+                try:
+                    n = int(input("\nSelect your number for the shape: "))
+                    if n > 1 and n <= 100:
+                        for i in range(n, -1, -1):
+                            for j in range(0, i + 1):
+                                print("*", end=" ")
+                            print()
+                        break
+                    else:
+                        print("\nEnter number greater than 1 and less than 100, so that my PC wont crash, Im poor")
+                        print("--------------------------------------Try again---------------------------------\n")
+                        continue
+                except ValueError:
+                    print("\nPlease enter a whole number between 1 - 100 only")
+                    print("------------------Try again---------------------\n")
+            break
+    #-----------------------------------------------------------------------------------------------------------
+        elif a == 1:
+            while True:
+                try:
+                    rows1 = int(input("\nSelect your number for the shape: "))
+                    if rows1 > 1 and rows1 <= 100:
+                        for i in range(0, rows1):
+                            for j in range(0, rows1 - i - 1):
+                                print(end=" ")
+                            for j in range(0, i + 1):
+                                print("*", end=" ")
+                            print()
+                        break
+                    else:
+                        print("\nEnter number greater than 1 and less than 100, so that my PC wont crash, Im poor")
+                        print("--------------------------------------Try again---------------------------------\n")
+                        continue
+                except ValueError:
+                    print("\nPlease enter a whole number between 1 - 100 only")
+                    print("------------------Try again---------------------\n")
             break
         else:
-            print("Enter number greater than 1 and less than 100, so that my PC wont crash, Im poor")
+            print("\n!!Enter 1 or 2 only!!\n")
             continue
     except ValueError:
-        print("Please enter a whole number between 1 - 100 only")
+        print("\nPlease enter 1 or 2 only")
+        print("--------Try again-------\n")
